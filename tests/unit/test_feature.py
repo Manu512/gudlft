ef9d304f146985b353ff1aaa10666f9c5976b60f
Emmanuel WALDNER <emmanuel.waldner@gmail.com>
2021-06-11 11:10:54 +0200

    improvement in testing

--- tests/unit/test_feature.py
+++ tests/unit/test_feature.py
@@ -11,9 +11,10 @@ competition = 'Fall Classic2'
 def test_competition_booking_message(clients):
     response = clients.post('/purchasePlaces', data={'club': club2, 'competition': competition, 'places': place})
     assert response.status_code == 200
-    assert b"Welcome, john@simplylift.co" in response.data
+    assert "Welcome, " + email2 in str(response.data)
     assert b"Great-booking complete ! You have reserved 5 places" in response.data
 
     response = clients.post('/purchasePlaces', data={'club': club1, 'competition': competition, 'places': place - 2})
-    assert b"Welcome, admin@irontemple.com" in response.data
+    assert "Welcome, " + email1 in str(response.data)
     assert b"Booking incomplete ! We are sorry, the competition is full !" in response.data
+
