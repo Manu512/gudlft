b049aeda5399062f52de7c6d58d14cb30a5d15a7
Emmanuel WALDNER <emmanuel.waldner@gmail.com>
2021-06-11 14:43:13 +0200

    Correction test case

--- tests/unit/test_bug1.py
+++ tests/unit/test_bug1.py
@@ -1,38 +1,38 @@
-import server
-
-server.app.config['TESTING'] = True
-client = server.app.test_client()
+import conftest
 
 
-def test_index():
-    response = client.get("/")
+def test_index(clients):
+    response = clients.get("/")
     assert response.status_code == 200
     assert b'Welcome to the GUDLFT Registration Portal!' in response.data
 
-def test_mail_know():
+
+def test_mail_know(clients):
     know_email = "admin@irontemple.com"
-    response = client.post("/", data={'email': know_email})
+    response = clients.post("/", data={'email': know_email})
     assert response.status_code == 200
     assert b'Points available:' in response.data
 
-def test_mail_unknow():
+
+def test_mail_unknow(clients):
     unknown_email = "mail@org.com"
-    response = client.post("/", data={'email': unknown_email})
+    response = clients.post("/", data={'email': unknown_email})
     assert response.status_code == 200
     assert b'Adresse email non autoris' in response.data
 
-def test_no_email():
+
+def test_no_email(clients):
     no_email = ""
-    response = client.post("/", data={'email': no_email})
+    response = clients.post("/", data={'email': no_email})
     assert response.status_code == 200
     assert b'Veuillez saisir une adresse email !' in response.data
 
-def test_logout():
-    test_mail_know()
-    response = client.get('/showSummary')
-    assert response.status_code == 200  # With session, I acces Summary
-    response = client.get('/logout')
-    assert response.status_code == 302  # Logout redirect to login
-    response = client.get('/showSummary')
-    assert response.status_code == 302  # After Logout Session is destroy, I must do login
 
+def test_logout(clients):
+    test_mail_know(clients)
+    response = clients.get('/showSummary')
+    assert response.status_code == 200  # With session, I acces Summary
+    response = clients.get('/logout')
+    assert response.status_code == 302  # Logout redirect to login
+    response = clients.get('/showSummary')
+    assert response.status_code == 302  # After Logout Session is destroy, I must do login
