3f3b86d54e33ccb2dc13b3bb201156a527baf6eb
Emmanuel WALDNER <emmanuel.waldner@gmail.com>
2021-06-25 09:30:15 +0200

    environment loader correction

--- env.bat
+++ env.bat
@@ -1,6 +1,6 @@
 @ECHO ON
 
-SET FLASK_APP="server.py"
+SET FLASK_APP=server.py
 SET PYTHONPATH=%cd%
 SET COVERAGE_RCFILE=%PYTHONPATH%\tests\.coveragerc
 SET LOCUST_LOCUSTFILE=%PYTHONPATH%\tests\locustfile.py
